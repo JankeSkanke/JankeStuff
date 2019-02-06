6d6a3a998dedb74dfc33c156f2593355469c05f7
Jan Ketil Skanke [MVP] <jan.ketil@outlook.com>
2019-02-06 20:52:44 +0100

    Add files via upload

---
+++ LabDeviceConfig.ps1
@@ -0,0 +1,15 @@
+﻿# Verify AzureAD Module
+Get-InstalledModule
+# Install AzureAD Module
+Install-Module AzureAD
+#
+#
+# Go to your resources folder C:\Resources 
+#
+# Here you should have "Chrome ADMX Ingestion.json" and "Chrome Configuration.json"
+#
+# To import this settings run DeviceConfiguration_Import_FromJSON.ps1 and point to these two configuration files
+
+./DeviceConfiguration_Import_FromJSON.ps1 
+
+#
