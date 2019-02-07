cf0d338c131584088fe8b85e8914be3d5055874a
Jan Ketil Skanke [MVP] <jan.ketil@outlook.com>
2019-02-07 22:33:51 +0100

    Create CleanupLab.ps1

---
+++ CleanupLab.ps1
@@ -0,0 +1,15 @@
+Break
+Install-Module MSOnline
+
+$Msolcred = Get-credential
+Connect-MsolService -Credential $MsolCred
+
+$domain = Get-msoldomain
+Set-MsolDirSyncEnabled -EnableDirSync $false -Force
+Get-MsolUser | Where-Object -Property UserPrincipalName -match "demodummies.com"
+#Stopp her om denne er blank for da er ikke domenet assignet
+
+Get-MsolUser | Where-Object -Property UserPrincipalName -match "demodummies.com"| Remove-MsolUser -force
+Set-MsolDomain -Name $domain.name[0] -IsDefault
+Remove-MsolDomain -DomainName $domain.name[1] -force
+
