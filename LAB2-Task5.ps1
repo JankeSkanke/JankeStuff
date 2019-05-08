b14dc9c495d40749a5e5b9a5ed41544a4a339cc3
Jan Ketil Skanke [MVP] <jan.ketil@outlook.com>
2019-05-08 14:01:42 +0200

    Add files via upload

---
+++ LAB2-Task5.ps1
@@ -0,0 +1,17 @@
+﻿Install-Module MSOnline
+Connect-MsolService
+
+$MySKUs = Get-MsolAccountSku
+$MyUsers = Get-MsolUser
+
+foreach ($user in $MyUsers){
+    if (($User.UserPrincipalName -notlike 'admin@*') -and ($User.IsLicensed -eq 'True')){
+        Write-Output "Removing license for user: " $user  
+        
+        foreach ($skuId in ($User.Licenses).AccountSkuID){
+            Write-Output "Removing SKU: " $skuid 
+            
+            Set-MsolUserLicense -ObjectId $user.ObjectId -RemoveLicenses $skuId
+            }
+        }
+}
---
+++ Lab02-Task1.ps1
@@ -0,0 +1,28 @@
+﻿# LAB 1 TASK 1 - Commands 
+
+# Assign your given UPN Suffix (given by instructor) 
+# (example: aaa.demodummies.com) 
+
+$UPNSuffix = "<INSERT SUFFIX HERE>"
+
+
+# This command will add the suffix to your local AD
+
+Set-ADForest -identity "corp.demodummies.com" -UPNSuffixes @{replace=$UPNSuffix}   
+
+# This command will add the suffix to the users who are in the OUs we are syncing later. 
+ 
+Get-ADUser -Filter * -SearchBase "OU=Users,OU=DemoDummies,DC=corp,DC=demodummies,DC=com" -Properties SamAccountName | ForEach-Object { Set-ADUser $_ -UserPrincipalName ($_.SamAccountName + $UPNSuffix )}   
+
+# This command will read the UPN and add this as the Proxy Adress and EMAIL attribute to our users 
+# (as we dont have a exchange server in our enviroment) 
+# NB This is not a supported way to handle mail attributes in production enviroments
+
+$Users = Get-ADUser -Filter * -SearchBase "OU=Users,OU=DemoDummies,DC=corp,DC=demodummies,DC=com" 
+
+foreach ($user in $Users)
+{
+	$Proxyaddress = "SMTP:"+$user.userprincipalname
+	Set-ADUser $user.samaccountname -replace @{ProxyAddresses=$Proxyaddress}
+    Set-ADuser $user.samaccountname -EmailAddress $user.UserPrincipalName
+}
